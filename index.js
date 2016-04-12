bc55aa85ea1d591e8bb599fcb7ff72af9c07968f
Zac Ioannidis <zacoppotamus@gmail.com>
2016-04-12 17:17:58 +0100

    Adding cases for Twilio

--- index.js
+++ index.js
@@ -2,20 +2,23 @@
 'use strict';
 
 var express = require('express');
-var Printer = require('thermalprinter');
-var serialport = require('serialport');
 var dotenv = require('dotenv');
 var ngrok = require('ngrok');
 
 var app = express();
+
+// Load environmental variables
+dotenv.load({ path: '.env' });
 
 // Controller for Express routes
 var routes = require('./routes');
 
 // Express configuration
 // function init() {
-  app.set('port', process.env.PORT || 4567);
-  app.get('/test', routes.ping);
+app.set('port', process.env.PORT || 4567);
+app.get('/test', routes.ping);
+app.get('/tokens', routes.getTokens);
+app.get('/print', routes.print);
 // }
 // module.exports.init = function() {
 
@@ -26,6 +29,7 @@ app.listen(app.get('port'), function() {
   console.log('running server on port: ', app.get('port'));
   ngrok.connect(app.get('port'), function (err, url) {
     console.log('ngrok url is: ', url);
+    process.env.PROCESS_URL = url;
   });
 });
 
--- routes.js
+++ routes.js
@@ -1,13 +1,71 @@
 var request = require('request');
+var querystring = require('querystring');
 
-// exports.getAuthenticationHeaders = function(req, res) {
-//   request.get(process.env.APP_URL + '/authenticate', function(err,request,body) {
+var SerialPort = require('serialport').SerialPort;
+var Printer = require('thermalprinter');
 
-//   })
-// }
+exports.getTokens = function(req, res) {
+  var query = querystring.stringify({
+    user_id: process.env.USER_ID
+  });
+
+  request.get(process.env.SERVER_URL + '/api/authenticate?' + query, function(err,request,body) {
+    console.log(process.env.SERVER_URL + '/api/authenticate?' + query);
+    if (err) {
+      res.status(401).send(err);
+    }
+    else {
+      res.status(200).send(JSON.parse(body));
+    }
+  });
+}
+
 
 exports.ping = function(req, res) {
   res.status(200).json({
     'status': 'ok'
   });
 };
+
+exports.print = function(req, res) {
+  var serialPort = new SerialPort(process.env.SERIAL_PORT, {
+    baudrate: 19200
+  })
+
+  var msgBody = req.query.Body.toLowerCase();
+
+  switch (msgBody) {
+    case 'summary':
+      console.log('summary requested');
+    case 'viz':
+      console.log('abstract visualization');
+    case 'test':
+      console.log('everything seems to be working');
+  }
+
+  serialPort.on('open',function() {
+      var printer = new Printer(serialPort);
+      printer.on('ready', function() {
+        printer
+          .horizontalLine(16)
+          .printLine('first line')
+          .bold(false)
+          .inverse(true)
+          .print(function() {
+            console.log('done');
+            res.status(200).json({
+              'status': 'ok'
+            });
+          });
+    });
+  });
+}
+
+// Set up Twilio authentication
+// Send message to serial
+// Get user settings for connected devices
+
+// A user adds their phone number on the settings page
+// When a user texts then the number is checked on main API server from DB
+// If theres a match with an account and tokens, a response is sent back.
+
